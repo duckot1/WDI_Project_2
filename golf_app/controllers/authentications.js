55659f9ad39d81f57d4af1135f331cebfe7f2806
duckot1 <tom.duckworth91@gmail.com>
2017-01-11 21:16:45 +0000

    about to start work on the app.js for the html

--- golf_app/controllers/authentications.js
+++ golf_app/controllers/authentications.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const config = require('../config/config');
 
 function authenticationsRegister(req, res){
   User.create(req.body.user, (err, user) => {
@@ -16,9 +18,11 @@ function authenticationsLogin (req, res) {
     if (!user || !user.validatePassword(req.body.password)) {
       return res.status(401).json({ message: 'Unauthorized.' });
     }
+    const token = jwt.sign(user._id, config.secret, { expiresIn: 60*60*24 });
     return res.status(200).json({
       message: 'Welcome back.',
-      user
+      user,
+      token
     });
   });
 }
--- golf_app/index.js
+++ golf_app/index.js
@@ -6,17 +6,24 @@ const bodyParser  = require('body-parser');
 const cors        = require('cors');
 const routes      = require('./config/routes');
 const webRoutes   = require('./config/webRoutes');
-const port        = process.env.PORT || 3000;
+const config      = require('./config/config');
+const expressJWT  = require('express-jwt');
 
-const databaseURL = process.env.MONGOLAB_URL || 'mongodb://localhost:27017/golf-app';
-mongoose.connect(databaseURL);
+mongoose.connect(config.db);
 
 app.use(express.static(`${__dirname}/public`));
 app.use(morgan('dev'));
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use('/api', expressJWT({ secret: config.secret })
+  .unless({
+    path: [
+      { url: '/api/login', methods: ['POST'] },
+      { url: '/api/register', methods: ['POST'] }
+    ]
+  }));
 app.use('/api', routes);
 app.use('/', webRoutes);
 
-app.listen(port, console.log(`Server has stated on port: ${port}`));
+app.listen(config.port, console.log(`Server has stated on port: ${config.port}`));
